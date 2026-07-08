a1039bbff29294805be150899b90c3400dbe57d7
zoeyskye2098-art <zoeyskye2098@gmail.com>
2026-07-08 23:37:12 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+    document.getElementById("results").innerHTML =
+        "<p>Search is not ready yet. We'll connect the full Pokémon database next.</p>";
+});
